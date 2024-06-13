d74b36a60ffcdfe1e165a418ade11ed31993008d
Anjana P <anjanap2410@gmail.com>
2024-06-13 12:40:24 +0530

    final

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './Movies.css'; 
+import './Movies.css';  
 
 const Movies = () => {
   const [title, setTitle] = useState('');
